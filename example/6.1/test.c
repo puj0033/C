55513ae2be9dd12c38d9c9a5ce70627fefac051d
6h057 <pujiun@gmail.com>
2017-05-14 20:13:06 +0800

    first commit

---
+++ example/6.1/test.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	int i, j, a;
+	a = 0;
+	for(i = 1; i <= 100; i++){
+			for(j = 2; j < i; j++){
+			 if(i % j == 0)
+					break;
+			}
+			if(i == j)
+				printf("%d is a prime.\n", i);
+			else
+				printf("%d is not a prime.\n", i);
+	}
+	return 0;
+}
---
+++ example/6.3/test.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	int i, j;
+	for(i = 1; i < 10; i++){
+			for(j = 1; j < 10; j++)
+			 	printf("%d\t", i*j);
+ 			printf("\n");
+	}
+	return 0;
+}
